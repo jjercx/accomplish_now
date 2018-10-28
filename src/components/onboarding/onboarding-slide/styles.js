6859e1af8ad88bc1f37c2617395a9aae52704876
sturmenta <sturmenta@gmail.com>
2018-10-27 22:54:43 -0300

    UX-fixes

--- src/components/onboarding/onboarding-slide/styles.js
+++ src/components/onboarding/onboarding-slide/styles.js
@@ -1,5 +1,5 @@
 import { widthPercentageToDP as wpd, heightPercentageToDP as hpd } from 'react-native-responsive-screen';
-import { HTP, WTP } from '../../../utils/dimensions';
+import { WTP } from '../../../utils/dimensions';
 import spacings from '../../spacing/styles';
 
 export default {
@@ -7,23 +7,13 @@ export default {
 		flex: 1,
 		alignItems: 'center'
 	},
-	logo: {
-		width: wpd( WTP( 192 ) ),
-		height: hpd( HTP( 105 ) )
-	},
 	wrapperOnboarding: {
-		bottom: hpd( '5%' ),
+		bottom: hpd( '25%' ),
 		position: 'absolute',
 		alignItems: 'center',
 		marginHorizontal: wpd( WTP( spacings.medium ) )
 	},
-	wrapperButtons: {
-		flexDirection: 'row'
-	},
 	wrapperSubTitle: {
 		height: hpd( '15%' )
-	},
-	wrapperButton: {
-		flex: 0.5
 	}
 };
--- src/screens/Onboarding.js
+++ src/screens/Onboarding.js
@@ -1,29 +1,65 @@
 import React, { Component } from 'react';
 import {
-	View
+	View, Image
 } from 'react-native';
 
 import Swiper from 'react-native-swiper';
+
+import {
+	widthPercentageToDP as wd,
+	heightPercentageToDP as hd
+} from 'react-native-responsive-screen';
+import { HTP, WTP } from '../utils/dimensions';
+
 import OnboardingSlide from '../components/onboarding/onboarding-slide/OnboardingSlide';
 import NavigatorPropType from '../types/navigator';
+import Spacing from '../components/spacing/Spacing';
+import spacings from '../components/spacing/styles';
+import Button from '../components/button/Button';
+import Typography from '../components/typography/Typography';
+import colors from '../theme/palette';
+
+const IMG_ONBOARDING1 = require( '../assets/images/onboardings/experts.png' );
+const IMG_ONBOARDING2 = require( '../assets/images/onboardings/meetings.png' );
+const IMG_ONBOARDING3 = require( '../assets/images/onboardings/workspaces.png' );
 
 const styles = {
-	wrapper: {
+	container: {
+		flex: 1
+	},
+	accomplishLogoContainer: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	wrapperOnboarding: {
+		position: 'absolute',
+		left: 0,
+		right: 0,
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		bottom: hd( '5%' ),
+		marginHorizontal: wd( WTP( spacings.medium ) )
 	},
 	slide: {
 		flex: 1
+	},
+	logo: {
+		width: wd( WTP( 192 ) ),
+		height: hd( HTP( 105 ) )
+	},
+	wrapperButtons: {
+		flexDirection: 'row'
+	},
+	wrapperButton: {
+		flex: 0.5
 	}
 };
-
-const TEXT_ONBOARDING = 'By continuing you are agreeing with our terms of service and privacy policy.';
-const TEXT_SUBTITLE1 = 'Get  smarter. Tap into the knowledge around of you.';
-const IMG_ONBOARDING1 = require( '../assets/images/onboardings/experts.png' );
-
-const TEXT_SUBTITLE2 = 'Get paid. Secure, in-app payments and down to the minute time tracking, get the most of your conversations.';
-const IMG_ONBOARDING2 = require( '../assets/images/onboardings/meetings.png' );
-
-const TEXT_SUBTITLE3 = 'Find your neighborhood’s most productive places to get your work done and take metings with experts.';
-const IMG_ONBOARDING3 = require( '../assets/images/onboardings/workspaces.png' );
 
 class Onboarding extends Component {
 	_onPressNewAccount() {
@@ -43,42 +79,67 @@ class Onboarding extends Component {
 	}
 
 	render() {
+		const screenData = {
+			slides: [
+				'Experts',
+				'Meetings',
+				'Workspaces'
+			],
+			TEXT_ONBOARDING: 'By continuing you are agreeing with our terms of service and privacy policy.',
+			TEXT_SUBTITLE1: 'Get  smarter. Tap into the knowledge around of you.',
+			IMG_ONBOARDING1,
+			TEXT_SUBTITLE2: 'Get paid. Secure, in-app payments and down to the minute time tracking, get the most of your conversations.',
+			IMG_ONBOARDING2,
+			TEXT_SUBTITLE3: 'Find your neighborhood’s most productive places to get your work done and take metings with experts.',
+			IMG_ONBOARDING3
+		};
+
 		return (
-			<Swiper style={styles.wrapper} showsPagination={false}>
-				<View style={styles.slide}>
-					<OnboardingSlide
-						text={TEXT_ONBOARDING}
-						title="Experts"
-						subTitle={TEXT_SUBTITLE1}
-						image={IMG_ONBOARDING1}
-						numSlide={1}
-						onPressNewAccount={() => this._onPressNewAccount()}
-						onPressSignIn={() => this._onPressSignIn()}
+			<View style={styles.container}>
+				<Swiper style={styles.wrapper} showsPagination={false}>
+					{screenData.slides.map( ( screenText, i ) => (
+						<View style={styles.slide} key={screenText}>
+							<OnboardingSlide
+								title={screenText}
+								subTitle={screenData[ `TEXT_SUBTITLE${i + 1}` ]}
+								image={screenData[ `IMG_ONBOARDING${i + 1}` ]}
+								numSlide={( i + 1 )}
+							/>
+						</View>
+					) )}
+				</Swiper>
+				<View style={styles.accomplishLogoContainer}>
+					<Spacing size="xLarge" />
+					<Image
+						style={styles.logo}
+						source={require( '../assets/images/logo-white/logo.png' )}
 					/>
 				</View>
-				<View style={styles.slide}>
-					<OnboardingSlide
-						text={TEXT_ONBOARDING}
-						title="Meetings"
-						subTitle={TEXT_SUBTITLE2}
-						image={IMG_ONBOARDING2}
-						numSlide={2}
-						onPressNewAccount={() => this._onPressNewAccount()}
-						onPressSignIn={() => this._onPressSignIn()}
-					/>
+				<View style={styles.wrapperOnboarding}>
+					<Spacing size="small" />
+					<View style={styles.wrapperButtons}>
+						<View style={styles.wrapperButton}>
+							<Button
+								text="Create account"
+								textColor={colors.blackLabels}
+								buttonColor={colors.white}
+								onPress={() => this._onPressNewAccount()}
+							/>
+						</View>
+						<Spacing size="small" horizontal />
+						<View style={styles.wrapperButton}>
+							<Button
+								text="Sign in"
+								textColor={colors.white}
+								buttonColor={colors.orange}
+								onPress={() => this._onPressSignIn()}
+							/>
+						</View>
+					</View>
+					<Spacing size="base" />
+					<Typography variant="smallBody" color="white">{screenData.TEXT_ONBOARDING}</Typography>
 				</View>
-				<View style={styles.slide}>
-					<OnboardingSlide
-						text={TEXT_ONBOARDING}
-						title="Workspaces"
-						subTitle={TEXT_SUBTITLE3}
-						image={IMG_ONBOARDING3}
-						numSlide={3}
-						onPressNewAccount={() => this._onPressNewAccount()}
-						onPressSignIn={() => this._onPressSignIn()}
-					/>
-				</View>
-			</Swiper>
+			</View>
 		);
 	}
 }
