add760f09c7f1f9b4e3f7f343d3056d2b83f8e61
Franco Correa <anibalfranciscocorrea@gmail.com>
2018-11-07 13:50:21 -0300

    Close 25 - Home without connections

--- src/components/home/my-connections/styles.js
+++ src/components/home/my-connections/styles.js
@@ -1,5 +1,5 @@
-import { heightPercentageToDP as hpd } from 'react-native-responsive-screen';
-import { HTP } from '../../../utils/dimensions';
+import { widthPercentageToDP as wpd, heightPercentageToDP as hpd } from 'react-native-responsive-screen';
+import { HTP, WTP } from '../../../utils/dimensions';
 
 export default {
 	container: {
@@ -8,5 +8,17 @@ export default {
 	wrapperConnections: {
 		marginLeft: hpd( HTP( 32 ) ),
 		top: hpd( HTP( 52 ) )
+	},
+	searchPerson: {
+		width: wpd( WTP( 20 ) ),
+		height: hpd( HTP( 20 ) ),
+		marginLeft: wpd( WTP( 20 ) )
+	},
+	containerLabel: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginTop: wpd( WTP( 30 ) ),
+		marginBottom: wpd( WTP( 20 ) ),
+		height: hpd( HTP( 50 ) )
 	}
 };
