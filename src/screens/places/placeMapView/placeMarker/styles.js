7a7d171a31bc965887021538a2bec786f1bcd00e
Luis <luis.chavez@tektonlabs.com>
2018-11-18 18:14:59 -0500

    maker add dimensions

--- src/screens/places/placeMapView/placeMarker/styles.js
+++ src/screens/places/placeMapView/placeMarker/styles.js
@@ -1,19 +1,25 @@
 import { StyleSheet } from 'react-native';
+import {
+	heightPercentageToDP as hp,
+	widthPercentageToDP as wp
+} from 'react-native-responsive-screen';
+
+import { HTP, WTP } from '../../../../utils/dimensions';
 
 export default StyleSheet.create( {
 	container: {
-		width: 80,
-		height: 80,
+		width: wp( WTP( 80 ) ),
+		height: hp( HTP( 80 ) ),
 		alignItems: 'center'
 	},
 	markerImage: {
 		flex: 1
 	},
 	placeImage: {
-		width: 50,
-		height: 50,
-		borderRadius: 50 / 2,
+		width: wp( WTP( 50 ) ),
+		height: hp( HTP( 50 ) ),
+		borderRadius: hp( HTP( 50 ) ) / 2,
 		position: 'absolute',
-		marginTop: 10
+		marginTop: hp( HTP( 10 ) )
 	}
 } );
